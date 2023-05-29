eb3cac4393c5649aed31528c0c94b6700ff5353d
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2023-05-29 15:58:37 -0300

    Add user page

--- src/components/UserListItem.tsx
+++ src/components/UserListItem.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import styles from './userListItem.module.sass'
 
 type Props = {
@@ -8,11 +9,13 @@ export function UserListItem({ user }: Props) {
   return (
     <div className={styles.container}>
       <h2>id: {user.id}</h2>
-      <h2>
-        {user.login.length > 12
-          ? user.login.substring(0, 11) + '...'
-          : user.login}
-      </h2>
+      <Link href={`/users/${user.login}`} style={{ textDecoration: 'none' }}>
+        <h2>
+          {user.login.length > 12
+            ? user.login.substring(0, 11) + '...'
+            : user.login}
+        </h2>
+      </Link>
     </div>
   )
 }
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -14,3 +14,15 @@ export const getUsersPage = async (page: number = 1, options = {}) => {
   )
   return resp.data
 }
+
+export const getUserByUsername = async (username: string) => {
+  const resp = await axios.get<GHUser>(
+    `https://api.github.com/users/${username}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_API_KEY}`
+      }
+    }
+  )
+  return resp.data
+}
